84e8f1a93385a25b42c10ca450b0b300f1ba14ae
Ada Rose Edwards <ada@ada.is>
2016-08-21 16:52:54 +0100

    path

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('port', (process.env.PORT || 3001));
 
 app.use('/', (req, res) => res.end('hello world'));
 
-app.use('/peerjs', ExpressPeerServer(app, {proxied: true, debug: true}));
+app.use('/peerjs/', ExpressPeerServer(app, {proxied: true, debug: true}));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
