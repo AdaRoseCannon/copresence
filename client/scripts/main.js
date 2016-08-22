cb4b970b82c2ee774f4a06025c8ab2a2987a29cb
Ada Rose Edwards <ada@ada.is>
2016-08-22 18:06:25 +0100

    autodetect port

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -2,5 +2,5 @@
 
 'use strict';
 
-const peer = new Peer({ path: 'peerjs', host: location.hostname, secure: !!location.protocol.match(/^https/) });
+const peer = new Peer({ path: 'peerjs', host: location.hostname, secure: !!location.protocol.match(/^https/), port: location.port || 80 });
 window.peer = peer;
